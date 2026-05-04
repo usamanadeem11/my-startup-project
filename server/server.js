a913906b7b35b973dca5d186ae675b756d982629
usamanadeem11 <mohdusama6666@gmail.com>
2026-05-04 18:42:24 +0530

    fix user model path

--- server/server.js
+++ server/server.js
@@ -64,3 +64,17 @@ app.get("/users", async (req, res) => {
   res.json(users);
 
 });
+
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  phone: String,
+  email: String,
+  location: String,
+  category: String,
+  service: String,
+  paymentId: String
+});
+
+module.exports = mongoose.model("User", userSchema);
